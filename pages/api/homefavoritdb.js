747a7e109b493a54f35fc88b8553cd41a44d561d
apisport <api.sport.team@gmail.com>
2022-05-31 14:00:39 +0700

    join

--- pages/api/homefavoritdb.js
+++ pages/api/homefavoritdb.js
@@ -7,6 +7,23 @@ async function getFavoritHome(req, res) {
         let { db } = await connectToDatabase();
         // fetch the posts
 
+        // let favorit = await db
+        //     .collection('favorit')
+        //     .find({})
+        //     .sort({ idfavorit: -1 })
+        //     .toArray();
+        // //[{"namaVenue": Scudetto, "alamat": Jalan WK}]
+        // let lapangan = await db
+        //     .collection('lapangan')
+        //     .find({})
+        //     .sort({ idfavorit: -1 })
+        //     .toArray();
+        // //[{"namaVenue": Scudetto, "namaLapangan": Lapangan 1}]
+        // const mergeById = favorit.map(itm => ({
+        //     ...lapangan.find((item) => (item.namaVenue === itm.namaVenue) && item),
+        //     ...itm
+        // }));
+        //[{"namaVenue": Scudetto, "alamat": Jawer, {"namaLapangan": Lapangan, "alamat"}}]
         let favoritHome = await db
             .collection('favorit')
             .aggregate([{
@@ -17,6 +34,7 @@ async function getFavoritHome(req, res) {
                     as: "lapanganVenue"
                 }
             }])
+            .toArray()
         // return the posts
         return res.json({
             message: JSON.parse(JSON.stringify(favoritHome)),
