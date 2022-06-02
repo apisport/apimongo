39d88c590f77bfd211e42f1764c1e448d14aa270
apisport <api.sport.team@gmail.com>
2022-06-02 14:54:46 +0700

    detail-lapangan

--- pages/detail-lapangan.js
+++ pages/detail-lapangan.js
@@ -20,28 +20,71 @@ export default function Home() {
     let lapangan = data['message']
     console.log(lapangan)
     let infoLapangan = lapangan[0]
-    // let namaHasil = lapangan.namaLapangan.split(" ").join("");
+    let namaHasil = infoLapangan.namaLapangan.split(" ").join("");
+
+    // Penggabungan Harga dan Jadwal
+    let keyJadwalPagi = Object.keys(infoLapangan.jadwalPagi)
+    let keyJadwalMalam = Object.keys(infoLapangan.jadwalMalam)
+    let gabunganJadwal = keyJadwalPagi.concat(keyJadwalMalam)
+    let gabunganHarga = []
+
+
+
+    for (let i = 0; i < keyJadwalPagi.length; i++) {
+        gabunganHarga.push(infoLapangan.hargaPagi)
+    }
+
+    for (let i = 0; i < keyJadwalMalam.length; i++) {
+        gabunganHarga.push(infoLapangan.hargaMalam)
+    }
+
+    //Penggabungan Harga dan Jadwal
+
+    const checkValue = () => {
+        let check = document.getElementsByName('jadwal')
+        for (let i = 0; i < check.length; i++){
+            console.log(`index ke-${i}`)
+            console.log(JSON.parse(check[i].value))
+        }
+        
+    }
     
     return (
-        <div className="container">
-            <h1 className='mb-3'>{infoLapangan.namaLapangan}</h1>
+        <div className="container mt-4">
+            <h1 className='mb-3 mt-4'>{infoLapangan.namaLapangan}</h1>
             <div className="row mb-3">
                 <div className="col md-3 mb-4">
                     {/* SLIDER */}
-                    <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
+                    <div id={`${namaHasil}`} className="carousel slide" data-bs-ride="carousel">
                         <div className="carousel-indicators">
-                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to={0} className="active" aria-current="true" aria-label="Slide 1" />
-                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to={1} aria-label="Slide 2" />
-                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to={2} aria-label="Slide 3" />
+                            {infoLapangan.gambar.map((data, i) => (
+                                <>
+                                    {i == 0 ?
+                                        (<button type="button" data-bs-target={`#${namaHasil}`} data-bs-slide-to={i} className="active" aria-current="true" aria-label={`Slide ${i}`} />) :
+                                        (<button type="button" data-bs-target={`#${namaHasil}`} data-bs-slide-to={i} aria-label={`Slide ${i}`} />)}
+
+                                </>
+                            ))}
                         </div>
                         <div className="carousel-inner">
-                            <Carousel />
+                            {infoLapangan.gambar.map((data, i) => (
+                                <>
+                                    {i == 0 ?
+                                        (<div className="carousel-item active">
+                                            <img src={`/uploads/${data}`} className="" width={400} height={200} />
+                                        </div>) :
+                                        (<div className="carousel-item">
+                                            <img src={`/uploads/${data}`} className="" width={400} height={200} />
+                                        </div>)}
+                                </>
+                            ))}
+
                         </div>
-                        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                        <button className="carousel-control-prev" type="button" data-bs-target={`#${namaHasil}`} data-bs-slide="prev">
                             <span className="carousel-control-prev-icon" aria-hidden="true" />
                             <span className="visually-hidden">Previous</span>
                         </button>
-                        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                        <button className="carousel-control-next" type="button" data-bs-target={`#${namaHasil}`} data-bs-slide="next">
                             <span className="carousel-control-next-icon" aria-hidden="true" />
                             <span className="visually-hidden">Next</span>
                         </button>
@@ -60,24 +103,32 @@ export default function Home() {
             </div>
             <div className='mt-3'>
                 <h4 className='text-start'>Jadwal Lapangan</h4>
-                <input type='date' className='form-control mb-4'></input>
+                <input type='date' id='tglMain' className='form-control mb-4'></input>
                 <div className='card p-3'>
                     <div className='row' style={{ color: 'white' }}>
-                        <CardJadwal />
-                        <CardJadwal />
-                        <CardJadwal />
-                        <CardJadwal />
-                        <CardJadwal />
-                        <CardJadwal />
-                        <CardJadwal />
-                        <CardJadwal />
-                        <CardJadwal />
-
+                        {/* THIS IS CARD */}
+                       
+                        {/* THIS IS CARD */}
+                        {gabunganJadwal.length === 0 ? (
+                            <h2>Tidak ada data Jadwal</h2>
+                        ) : (
+                            <>
+                                {gabunganJadwal.map((data, index) => (
+                                    <div className='col-6 col-lg-3 mb-2'>
+                                        <div>
+                                            <input type="checkbox" className="btn-check" id={`btn-check${index+1}`} autoComplete="off" name='jadwal' value={JSON.stringify([`${data}`, gabunganHarga[index]])} />
+                                            <label className="btn btn-outline-primary" htmlFor={`btn-check${index + 1}`}>{data}<br />{`Rp ${gabunganHarga[index]}.000`}</label><br />
+                                        </div>
+                                    </div>
+                                ))}
+                            </>
+                        )}
                     </div>
                 </div>
+                
 
                 <div className='row'>
-                    <a className='btn btn-fill text-white mt-3' href='/pembayaran'>Pesan</a>
+                    <button type='button' className='btn btn-fill text-white mt-3' onClick={checkValue}>Pesan</button>
                 </div>
 
             </div>
